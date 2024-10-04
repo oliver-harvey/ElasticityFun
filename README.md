3552b2a63625e52292c43886acf1a6db83b7dcff
Oli Harvey <oliver.harvey@mpb.com>
2024-10-04 16:52:21 +0100

    test app setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ElasticityFun
+
+This is a dev friday afternoon project to produce interactive tools to explore our understanding of price elasticity.
+
+Note this is entirely built with toy data and should not be seen as indicative of real life economics.
---
+++ app.py
@@ -0,0 +1,21 @@
+import dash
+from dash import dcc, html
+import plotly.graph_objects as go
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                go.Scatter(x=[1, 2, 3, 4], y=[10, 11, 12, 13], mode='lines', name='Buy Units'),
+                go.Scatter(x=[1, 2, 3, 4], y=[15, 14, 13, 12], mode='lines', name='Sell Units')
+            ],
+            'layout': go.Layout(title='Units Sold and Bought vs Price', xaxis={'title': 'Price'}, yaxis={'title': 'Units'})
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
