b4b59736821cc78d02df1a0dac1961f35804c557
Oli Harvey <oliver.harvey@mpb.com>
2024-10-04 17:17:47 +0100

    amended main

--- app.py
+++ app.py
@@ -17,5 +17,6 @@ app.layout = html.Div([
     )
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
+
